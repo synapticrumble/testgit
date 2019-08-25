e956e92fb79341b6bdac10426163769b55b6ccf6
synapticrumble@yahoo.com <synapticrumble@yahoo.com>
2019-08-24 23:04:23 -0500

    added to my testgit on 24th Aug 2019

--- fun.py
+++ fun.py
@@ -1,4 +1,10 @@
+print "how are you doing today"
+print " "
+print "hello world!!!"
+# these are all 
+# comment lines
+# which will be tested for upstream upload in github
 
-print hello world!!!
+
 
 
---
+++ readme.txt
@@ -0,0 +1,1 @@
+This is a test text file being uploaded in github
